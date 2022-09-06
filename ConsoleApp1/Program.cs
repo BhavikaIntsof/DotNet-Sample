643d3e8ca3e107add4bbbb596488f5293c757ac4
BhavikaIntsof <bhavika.bhagchandani@intsof.com>
2022-09-06 11:54:50 +0530

    Culture Info and DateTime info

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Globalization;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CultureInfo culture = new CultureInfo("en-US");
+            Console.WriteLine(culture.DateTimeFormat);
+            string currentCulture = Thread.CurrentThread.CurrentCulture.DisplayName;
+            DateTime dateTime = DateTime.Now;
+            string dateInUSA = dateTime.ToString( new CultureInfo("en-us"));
+            Console.WriteLine(dateInUSA);
+            string dateInIndia = dateTime.ToString(new CultureInfo("hi-IN"));
+            Console.WriteLine(dateInIndia);
+            string dateInFrench = dateTime.ToString("D",new CultureInfo("fr-FR"));
+            Console.WriteLine(dateInFrench);
+        }
+    }
+}
